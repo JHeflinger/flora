44be0bf571b205da8ac6dad1551dbd67737b4af3
JHeflinger <jasonheflinger@gmail.com>
2023-01-22 22:18:57 -0500

    set up premake

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Flora"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Flora"
+    location "Flora"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FL_PLATFORM_WINDOWS",
+            "FL_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "FL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "FL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "FL_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "Consoleapp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Flora/vendor/spdlog/include",
+        "Flora/src"
+    }
+
+    links
+    {
+        "Flora"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FL_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "FL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "FL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "FL_DIST"
+        optimize "On"
