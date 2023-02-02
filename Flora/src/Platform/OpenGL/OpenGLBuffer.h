0ef72fb7fc255eb6a67586de2564f047e49e6f07
JHeflinger <jasonheflinger@gmail.com>
2023-02-02 15:23:43 -0500

    added vertex buffer layouts

--- Flora/src/Platform/OpenGL/OpenGLBuffer.h
+++ Flora/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,16 +8,19 @@ namespace Flora {
 		virtual ~OpenGLVertexBuffer();
 		virtual void Bind() const;
 		virtual void Unbind() const;
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
 	public:
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 		virtual uint32_t GetCount() const { return m_Count;	};
 	private:
 		uint32_t m_RendererID;
