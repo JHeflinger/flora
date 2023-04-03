c381186e9a3dbd7bc963b01ff8f1e8891bbf43fd
JHeflinger <jasonheflinger@gmail.com>
2023-04-03 19:02:16 -0400

    maintenance

--- Fauna/src/EditorLayer.cpp
+++ Fauna/src/EditorLayer.cpp
@@ -144,7 +144,7 @@ namespace Flora {
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
 		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
-		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+		ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		ImGui::End();
 		ImGui::PopStyleVar();
 
