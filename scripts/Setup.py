d3f3ef058cec90406274fd7fb327530c70878379
Jason <jasonheflinger@gmail.com>
2023-04-07 23:24:25 -0400

    reverted vulkan setup

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -10,11 +10,11 @@ import Vulkan
 # Change from Scripts directory to root
 os.chdir('../')
 
-if (not Vulkan.CheckVulkanSDK()):
-    print("Vulkan SDK not installed.")
+#if (not Vulkan.CheckVulkanSDK()):
+#    print("Vulkan SDK not installed.")
 
-if (not Vulkan.CheckVulkanSDKDebugLibs()):
-    print("Vulkan SDK debug libs not found.")
+#if (not Vulkan.CheckVulkanSDKDebugLibs()):
+#    print("Vulkan SDK debug libs not found.")
 
 print("Running premake...")
 subprocess.call(["vendor/premake/bin/premake5.exe", "vs2019"])
