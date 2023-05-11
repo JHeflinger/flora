ede9069402ed32d95ae931a54f760d7b111cca85
JHeflinger <jasonheflinger@gmail.com>
2023-05-11 12:38:05 -0400

    filled out basic asset manager

--- Flora/src/Flora/Renderer/AssetManager.cpp
+++ Flora/src/Flora/Renderer/AssetManager.cpp
@@ -11,18 +11,19 @@ namespace Flora {
 	}
 
 	bool AssetManager::AddTexture(std::string texPath) {
-
+		if (m_TextureMap.find(texPath) == m_TextureMap.end()) return false;
+		m_TextureMap[texPath] = Texture2D::Create(texPath);
 	}
 
 	void AssetManager::RemoveTexture(std::string texPath) {
-
+		m_TextureMap.erase(texPath);
 	}
 
 	void AssetManager::ClearTextures() {
-
+		m_TextureMap.clear();
 	}
 
 	Ref<Texture2D> AssetManager::GetTexture(std::string texPath) {
-
+		return m_TextureMap[texPath];
 	}
 }
