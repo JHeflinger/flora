8b33d3a17621fdd85456153b571d33234f5c5f3a
JHeflinger <jasonheflinger@gmail.com>
2023-05-11 11:58:14 -0400

    started assetmanager

---
+++ Flora/src/Flora/Renderer/AssetManager.cpp
@@ -0,0 +1,28 @@
+#include "flpch.h"
+#include "Flora/Renderer/AssetManager.h"
+
+namespace Flora {
+	AssetManager::AssetManager() {
+
+	}
+
+	AssetManager::~AssetManager() {
+		// note: might be a minor memory leak here?
+	}
+
+	bool AssetManager::AddTexture(std::string texPath) {
+
+	}
+
+	void AssetManager::RemoveTexture(std::string texPath) {
+
+	}
+
+	void AssetManager::ClearTextures() {
+
+	}
+
+	Ref<Texture2D> AssetManager::GetTexture(std::string texPath) {
+
+	}
+}
