be06059da97ac112ca5a03efac802084903da6cd
Jason Heflinger <jasonheflinger@gmail.com>
2023-10-24 18:08:21 -0500

    updated docs

--- Fauna/docs/html/search/all_a.js
+++ Fauna/docs/html/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['linearvelocity_0',['LinearVelocity',['../class_flora_1_1_rigid_body2_d_component.html#a224ed8894b601d2c174577a51d065fdc',1,'Flora::RigidBody2DComponent']]],
-  ['localinput_1',['LocalInput',['../class_flora_1_1_local_input.html',1,'Flora']]]
+  ['labelcomponent_0',['LabelComponent',['../class_flora_1_1_label_component.html',1,'Flora']]],
+  ['linearvelocity_1',['LinearVelocity',['../class_flora_1_1_rigid_body2_d_component.html#a224ed8894b601d2c174577a51d065fdc',1,'Flora::RigidBody2DComponent']]],
+  ['localinput_2',['LocalInput',['../class_flora_1_1_local_input.html',1,'Flora']]]
 ];
