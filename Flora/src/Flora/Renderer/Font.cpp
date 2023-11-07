8c3e042b861cac6735c5b8c335055b5d7408d2b5
Jason Heflinger <jasonheflinger@gmail.com>
2023-11-06 22:41:00 -0600

    got font work started

--- Flora/src/Flora/Renderer/Font.cpp
+++ Flora/src/Flora/Renderer/Font.cpp
@@ -1,8 +1,29 @@
 #include "flpch.h"
 #include "Font.h"
+#undef INFINITE // <- man idk fix this later but its mainly in issue in windows
 #include "msdf-atlas-gen.h"
 #include "msdfgen.h"
 
 namespace Flora {
-
+	Font::Font(const std::filesystem::path& filepath) {
+        msdfgen::FreetypeHandle* ft = msdfgen::initializeFreetype();
+        if (ft) {
+            msdfgen::FontHandle* font = msdfgen::loadFont(ft, filepath.string().c_str());
+            if (font) {
+                msdfgen::Shape shape;
+                if (msdfgen::loadGlyph(shape, font, 'A')) {
+                    shape.normalize();
+                    //                      max. angle
+                    msdfgen::edgeColoringSimple(shape, 3.0);
+                    //           image width, height
+                    msdfgen::Bitmap<float, 3> msdf(32, 32);
+                    //                     range, scale, translation
+                    msdfgen::generateMSDF(msdf, shape, 4.0, 1.0, msdfgen::Vector2(4.0, 4.0));
+                    msdfgen::savePng(msdf, "output.png");
+                }
+                msdfgen::destroyFont(font);
+            }
+            msdfgen::deinitializeFreetype(ft);
+        }
+	}
 }
